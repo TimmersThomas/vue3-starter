8ade54c1fe8cc6d04ee5c7de7beafb1b7b4f42cf
Thomas Timmers <me@timmersthomas.be>
2021-04-26 10:09:21 +0200

    VITE integration

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+  },
+  plugins: [
+  ],
+}
